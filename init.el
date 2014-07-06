5b3182c2a3d0533c517717991c90ad3d1c2cc9ff
Anup pokhrel <virtualanup@gmail.com>
2014-07-06 18:22:13 +0545

    Added multiple cursors

--- init.el
+++ init.el
@@ -97,3 +97,11 @@ This follows freedesktop standards, should work in X servers."
 (global-set-key (kbd "C-c u") 'uncomment-region)
 
 (global-set-key (kbd "C-c C-k") 'eval-buffer)
+
+;;add multiple cursor module
+(add-to-list 'load-path "~/.emacs.d/multiple-cursors.el")
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
