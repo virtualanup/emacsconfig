614babe47aee39b09b078af253612288f1dcce46
Anup pokhrel <virtualanup@gmail.com>
2014-08-10 22:42:17 +0545

    Changed session function names

--- sessions.el
+++ sessions.el
@@ -8,23 +8,23 @@
 (defvar session-name-history nil
   "Desktop session name history")
 
-(defun session-save (&optional name)
+(defun ss (&optional name)
   "Save desktop with a name."
   (interactive)
   (unless name
-    (setq name (session-name "Save session as: ")))
+    (setq name (sessionname "Save session as: ")))
   (make-directory (concat session-dir name) t)
   (desktop-save (concat session-dir name) t)
 )
 
-(defun session-load (&optional name)
+(defun sl (&optional name)
   "Read desktop with a name."
   (interactive)
   (unless name
-    (setq name (session-name "Load session: ")))
+    (setq name (sessionname "Load session: ")))
   (desktop-read (concat session-dir name)))
 
-(defun session-name (prompt)
+(defun sessionname (prompt)
   (completing-read prompt (and (file-exists-p session-dir)
                                (directory-files session-dir))
                    nil nil nil session-name-history))
