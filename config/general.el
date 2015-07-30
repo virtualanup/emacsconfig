9a172b8f9fb1266cb0f7889543d6ea7e178ffa57
virtualanup <virtualanup@gmail.com>
2015-07-30 19:24:13 +0545

    Removed virtual buffer in ido mode

--- config/general.el
+++ config/general.el
@@ -44,8 +44,6 @@ ido-enable-flex-matching t ;; enable fuzzy matching
 ido-auto-merge-work-directories-length nil
 ido-create-new-buffer 'always
 ido-use-filename-at-point 'guess
-ido-use-virtual-buffers t
-ido-handle-duplicate-virtual-buffers 2
 ido-max-prospects 10
 )
 
